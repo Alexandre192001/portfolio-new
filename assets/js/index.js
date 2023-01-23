c194089dc284b4b2b44f091abff56e6ba287bbc7
Alexandre Júnior <alexandrepj13@gmail.com>
2023-01-23 15:27:06 -0300

    Finnaly responsive and carousel

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,9 +8,15 @@ var portfolio = document.querySelector(".container-portfolio")
 var contato = document.querySelector(".container-contact")
 var footer = document.querySelector(".container-footer")
 
+darkLigth.addEventListener("click", function () {
 
-darkLigth.addEventListener("click", function(){
-    if(this.classList.toggle("bi-brightness-high-fill")){
+    const removeMenu = document.querySelector(".navigation")
+    removeMenu.classList.remove("menuspeed")
+    document.querySelector(".menu").classList.remove("bi-x-lg")
+    document.querySelector(".menu").classList.add("bi-list")
+
+    if (this.classList.toggle("bi-brightness-high-fill")) {
+
         this.classList.remove("bi-moon-stars-fill")
         header.classList.add("dark")
         navbar.classList.add("dark")
@@ -18,8 +24,9 @@ darkLigth.addEventListener("click", function(){
         skill.classList.add("dark")
         portfolio.classList.add("dark")
         contato.classList.add("dark")
-       
-    } else if(this.classList.toggle("bi-moon-stars-fill")){
+        
+    } else if (this.classList.toggle("bi-moon-stars-fill")) {
+        
         this.classList.remove("bi-brightness-high-fill")
         header.classList.remove("dark")
         navbar.classList.remove("dark")
@@ -27,52 +34,83 @@ darkLigth.addEventListener("click", function(){
         skill.classList.remove("dark")
         portfolio.classList.remove("dark")
         contato.classList.remove("dark")
-    
+       
     }
+
 })
 
 const btnArrowHeigthCard = document.querySelectorAll(".bi-arrow-down-square")
-btnArrowHeigthCard.forEach((btnArrow)=>{
+btnArrowHeigthCard.forEach((btnArrow) => {
     btnArrow.addEventListener("click", HeigthCard)
 })
-function HeigthCard(element){
+
+function HeigthCard(element) {
     const cardElemento = element.target
     let cardHeigth = cardElemento.closest(".card-skill")
 
-    if(this.classList.toggle("bi-x-circle")){
+    if (this.classList.toggle("bi-x-circle")) {
         this.classList.remove("bi-arrow-down-square")
         cardHeigth.classList.add("heigthCard")
 
-    } else if(this.classList.toggle("bi-arrow-down-square")) {
+    } else if (this.classList.toggle("bi-arrow-down-square")) {
         this.classList.remove("bi-x-circle")
         cardHeigth.classList.remove("heigthCard")
     }
-    
+
 }
 
 
 const menu = document.querySelector(".bi-list")
 menu.addEventListener("click", menuSpeed)
 
-function menuSpeed(){
-    if(this.classList.toggle("bi-x-lg")){
+function menuSpeed() {
+    if (this.classList.toggle("bi-x-lg")) {
         this.classList.remove("bi-list")
         document.querySelector(".navigation").classList.add("menuspeed")
-    } else if(this.classList.toggle("bi-list")){
+    } else if (this.classList.toggle("bi-list")) {
         this.classList.remove("bi-x-lg")
         document.querySelector(".navigation").classList.remove("menuspeed")
     }
 }
 
 const navigations = document.querySelectorAll(".navigation a")
-navigations.forEach((a)=>{
-    a.addEventListener("click",removeMenuClick)
+navigations.forEach((a) => {
+    a.addEventListener("click", removeMenuClick)
 })
 
-function removeMenuClick(e){
+function removeMenuClick(e) {
     const removeMenu = e.target
     const remove = removeMenu.closest(".navigation")
     remove.classList.remove("menuspeed")
     document.querySelector(".menu").classList.remove("bi-x-lg")
     document.querySelector(".menu").classList.add("bi-list")
 }
+
+let carouselItems = document.querySelectorAll(".carousel-items")
+const rightCarousel = document.querySelector(".bi-arrow-right-circle-fill")
+const leftCarousel = document.querySelector(".bi-arrow-left-circle-fill")
+let currentIndexCarousel = 0
+
+rightCarousel.addEventListener("click", function () {
+    if (currentIndexCarousel === carouselItems.length - 1) {
+        currentIndexCarousel = 0
+    } else {
+        currentIndexCarousel++
+    }
+    carouselItems.forEach((elemento) => {
+        elemento.classList.remove("hidden")
+    })
+    carouselItems[currentIndexCarousel].classList.add("hidden")
+})
+
+leftCarousel.addEventListener("click", function () {
+    if (currentIndexCarousel === 0) {
+        currentIndexCarousel = carouselItems.length - 1
+    } else {
+        currentIndexCarousel--
+    }
+    carouselItems.forEach((elemento) => {
+        elemento.classList.remove("hidden")
+    })
+    carouselItems[currentIndexCarousel].classList.add("hidden")
+})
